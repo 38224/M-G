3daa9d62b089de8ddd02ffdd8e40870873d40523
38224 <38224@alunos.isel.ipl.pt>
2017-07-18 23:48:04 +0100

    projeto

---
+++ PokeM-G/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokeM_G.Startup))]
+namespace PokeM_G
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
